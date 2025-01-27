ab283565ba5b9f9079e325a8c028c94279e5229a
on9au <151502370+on9au@users.noreply.github.com>
2025-01-27 18:55:59 +1100

    Consolidate submission files into a single file for Task 2b

--- task_2b/task_2b.py
+++ task_2b/task_2b.py
@@ -1,3 +1,9 @@
+"""VSV Applied Computing U3W2 Task 2b Submission File
+All the files have been combined into a single file for submission.
+
+@Author: Dylan Jong
+"""
+
 test_string = "The quick brown fox jumps over the lazy dog"
 
 # Print the original string
